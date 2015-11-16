bb8a20ffb9768f3d257ad3d8793952bceaf4da3f
Nishant Totla <nishanttotla@gmail.com>
2015-11-15 19:04:22 -0800

    More structures for new algorithm

--- src/BetterFCFS.h
+++ src/BetterFCFS.h
@@ -10,7 +10,9 @@
     > it elevator doesn't have a direction, but has pending dropoffs, then pick a direction and add dropoffs in that direction to list of destinations
 [at this stage, each elevator has a direction, or no destination at all]
 - For each pickup request
-    > add it to the list of destinations for an elevator that has an empty list, or is moving in the direction of the requested floor, with the requested direction being the same
+    > add it to the list of destinations for an elevator that is moving in the direction of the requested floor, with the requested direction being the same
+    > if not found, add it to the list of destionation for an elevator that has an empty list
+    (no load balancing happening here)
 - For each elevator
     > update destination to closest floor in the direction of movement
 */
@@ -19,12 +21,20 @@ struct ExtendedElevatorState {
     std::set<int> destinations;
 };
 
+// struct used to check if a given floor has up/down requests
+struct FloorRequest {
+    bool up;
+    int upTimestamp; // timestamp for oldest up request, only counts if up == true
+    bool down;
+    int downTimestamp; // timestamp for oldest down request, only counts if down == true
+};
+
 class BetterFCFS : public ElevatorManager {
   private:
     // extended state information
     std::vector<ExtendedElevatorState> extendedState; // extended state for all elevators
     // queue to store pickup requests
-    std::deque<PickupRequest> pickupRequests;
+    std::vector<FloorRequest> pickupRequests;
   public:
     FCFS(int count, int floors);
     void pickup(int floor, int direction);
